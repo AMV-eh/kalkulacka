9338c954f165166697957af4c9148b9ef2bb3ae3
LiptonZIntru <namhoangng.nguyenn@gmail.com>
2022-04-12 23:32:14 +0200

    Cleanup code

--- src/Calculator/Calculator/MainWindow.xaml.cs
+++ src/Calculator/Calculator/MainWindow.xaml.cs
@@ -24,10 +24,5 @@ namespace Calculator
         {
             InitializeComponent();
         }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
     }
 }
