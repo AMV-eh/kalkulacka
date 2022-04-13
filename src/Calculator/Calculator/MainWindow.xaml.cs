f25fbf9ad3fa32eafd34ddb7f0ff3407abce0eb3
LiptonZIntru <namhoangng.nguyenn@gmail.com>
2022-04-13 17:55:09 +0200

    Added basic structure

--- src/Calculator/Calculator/MainWindow.xaml.cs
+++ src/Calculator/Calculator/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Calculator.Models;
+using MathFunctions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,33 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MathFunction MathFunction { get; set; }
+        private CalcAction CalcAction { get; set; }
         public MainWindow()
         {
+            MathFunction = new MathFunction();
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            resultTextBox.Text += button.Content;
+        }
+
+        private void Button_Click_Factorial(object sender, RoutedEventArgs e)
+        {
+            int a;
+            int.TryParse(resultTextBox.Text, out a);
+
+            int result = MathFunction.Factorial(a);
+
+            resultTextBox.Text = $"{result}";
+        }
+
+        private void Button_Plus_Click(object sender, RoutedEventArgs e)
+        {
+            CalcAction = CalcAction.Plus;
         }
     }
 }
---
+++ src/Calculator/Calculator/Models/CalcAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Models
+{
+    
+    public enum CalcAction
+    {
+        Plus = 0,
+        Minus = 1,
+        Multiplication = 2,
+        Division = 3
+    }
+}
